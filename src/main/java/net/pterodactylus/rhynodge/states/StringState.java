63752b7b280e33a6a7a9e76b3d5a147f9f1b332a
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2013-02-26 21:50:18 +0100

    Add state that stores a single String value.

---
+++ src/main/java/net/pterodactylus/rhynodge/states/StringState.java
@@ -0,0 +1,50 @@
+/*
+ * rhynodge - StringState.java - Copyright © 2013 David Roden
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.pterodactylus.rhynodge.states;
+
+/**
+ * A {@link net.pterodactylus.rhynodge.State} that stores a single {@link
+ * String} value.
+ *
+ * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
+ */
+public class StringState extends AbstractState {
+
+	/** The string value. */
+	private final String value;
+
+	/**
+	 * Creates a new string state.
+	 *
+	 * @param value
+	 * 		The value of the state
+	 */
+	public StringState(String value) {
+		this.value = value;
+	}
+
+	/**
+	 * Returns the string value of this state.
+	 *
+	 * @return The string value
+	 */
+	public String value() {
+		return value;
+	}
+
+}
