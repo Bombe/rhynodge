5bf8e42840cbb9a3d063b07d7e17b723331296dc
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2013-05-03 18:56:39 +0200

    Add comic filter and watcher for SMBC.

---
+++ src/main/java/net/pterodactylus/rhynodge/filters/comics/SaturdayMorningBreakfastCerealComicFilter.java
@@ -0,0 +1,61 @@
+/*
+ * rhynodge - SaturdayMorningBreakfastCerealComicFilter.java - Copyright © 2013 David Roden
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.pterodactylus.rhynodge.filters.comics;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import net.pterodactylus.rhynodge.filters.ComicSiteFilter;
+
+import com.google.common.base.Optional;
+import com.google.common.base.Predicate;
+import com.google.common.collect.FluentIterable;
+import org.jsoup.nodes.Document;
+
+/**
+ * TODO
+ *
+ * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
+ */
+public class SaturdayMorningBreakfastCerealComicFilter extends ComicSiteFilter {
+
+	@Override
+	protected Optional<String> extractTitle(Document document) {
+		return Optional.of("");
+	}
+
+	@Override
+	protected List<String> extractImageUrls(Document document) {
+		String imageUrl = document.select(".comicboxcenter img:eq(1)").attr("src");
+		String afterImageUrl = document.select("#aftercomic img").attr("src");
+		return FluentIterable.from(Arrays.asList(imageUrl, afterImageUrl)).filter(new Predicate<String>() {
+
+			@Override
+			public boolean apply(String url) {
+				return url.length() > 0;
+			}
+		}).toList();
+	}
+
+	@Override
+	protected List<String> extractImageComments(Document document) {
+		return Collections.emptyList();
+	}
+
+}
