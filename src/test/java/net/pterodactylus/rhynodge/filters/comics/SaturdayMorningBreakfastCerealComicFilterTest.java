8c6fbacda7fb8a47533b3d9c42653949527dfb0c
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2015-05-04 20:20:05 +0200

    Add test for SMBC

---
+++ src/test/java/net/pterodactylus/rhynodge/filters/comics/SaturdayMorningBreakfastCerealComicFilterTest.java
@@ -0,0 +1,51 @@
+package net.pterodactylus.rhynodge.filters.comics;
+
+import java.io.IOException;
+
+import net.pterodactylus.rhynodge.Filter;
+import net.pterodactylus.rhynodge.State;
+import net.pterodactylus.rhynodge.filters.ResourceLoader;
+import net.pterodactylus.rhynodge.states.ComicState;
+import net.pterodactylus.rhynodge.states.ComicState.Comic;
+import net.pterodactylus.rhynodge.states.HtmlState;
+
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
+import org.jsoup.nodes.Document;
+import org.junit.Test;
+
+/**
+ * Unit test for {@link SaturdayMorningBreakfastCerealComicFilter}.
+ *
+ * @author <a href="mailto:bombe@pterodactylus.net">David ‘Bombe’ Roden</a>
+ */
+public class SaturdayMorningBreakfastCerealComicFilterTest {
+
+	private final Filter smbcFilter = new SaturdayMorningBreakfastCerealComicFilter();
+	private final HtmlState htmlState;
+
+	public SaturdayMorningBreakfastCerealComicFilterTest() throws IOException {
+		Document document = ResourceLoader.loadDocument(SinfestComicFilter.class, "saturday-morning-breakfast-cereal.html", "http://www.smbc-comics.com/");
+		htmlState = new HtmlState("http://www.smbc-comics.com/", document);
+	}
+
+	@Test
+	public void htmlCanBeParsed() {
+		State state = smbcFilter.filter(htmlState);
+		MatcherAssert.assertThat(state, Matchers.instanceOf(ComicState.class));
+	}
+
+	@Test
+	public void comicIsParsedCorrectly() {
+		ComicState comicState = (ComicState) smbcFilter.filter(htmlState);
+		MatcherAssert.assertThat(comicState.comics(), Matchers.hasSize(1));
+		Comic comic = comicState.comics().get(0);
+		MatcherAssert.assertThat(comic.title(), Matchers.is(""));
+		MatcherAssert.assertThat(comic.strips(), Matchers.hasSize(2));
+		MatcherAssert.assertThat(comic.strips().get(0).imageUrl(), Matchers.is("http://www.smbc-comics.com/comics/1430750631-20150504.png"));
+		MatcherAssert.assertThat(comic.strips().get(0).comment(), Matchers.is("And they say there's no use for a liberal arts degree!"));
+		MatcherAssert.assertThat(comic.strips().get(1).imageUrl(), Matchers.is("http://smbc-comics.com/comics/1430750631-20150504after.png"));
+		MatcherAssert.assertThat(comic.strips().get(1).comment(), Matchers.is(""));
+	}
+
+}
