ecc5dee21c4093a2dec2df06f6dc4a0c7f85edf7
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2016-05-27 07:35:38 +0200

    Add watcher that gets Hamburg’s weather

---
+++ src/main/kotlin/net/pterodactylus/rhynodge/webpages/weather/wettercom/WetterComHamburgWatcher.kt
@@ -0,0 +1,15 @@
+package net.pterodactylus.rhynodge.webpages.weather.wettercom
+
+import net.pterodactylus.rhynodge.queries.HttpQuery
+import net.pterodactylus.rhynodge.watchers.DefaultWatcher
+
+/**
+ * [DefaultWatcher] implementation that gets the weather for Hamburg.
+ *
+ * @author [David ‘Bombe’ Roden](mailto:bombe@pterodactylus.net)
+ */
+class WetterComHamburgWatcher : DefaultWatcher(
+        HttpQuery("http://www.wetter.com/wetter_aktuell/wettervorhersage/heute/deutschland/hamburg/DE0004130.html"),
+        listOf(WetterComFilter()),
+        WetterComTrigger()
+)
