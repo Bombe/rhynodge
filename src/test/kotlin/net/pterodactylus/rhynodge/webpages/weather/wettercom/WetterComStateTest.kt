a0ae448c1c3095d2dd8a6b4249e247f760cf9cd2
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2016-05-27 07:25:34 +0200

    Remove debug output

--- src/test/kotlin/net/pterodactylus/rhynodge/webpages/weather/wettercom/WetterComStateTest.kt
+++ src/test/kotlin/net/pterodactylus/rhynodge/webpages/weather/wettercom/WetterComStateTest.kt
@@ -17,7 +17,6 @@ class WetterComStateTest {
     @Test
     fun statesWithoutHoursEqualOneAnother() {
         val now = Instant.now().atZone(ZoneId.of("UTC"))
-        println("%s %s".format(now, now.javaClass))
         val firstState = WetterComState(ZonedDateTime.from(now))
         val secondState = WetterComState(ZonedDateTime.from(now))
         assertThat(firstState, `is`(secondState))
@@ -26,7 +25,6 @@ class WetterComStateTest {
     @Test
     fun statesWithTheSameHoursAreEqual() {
         val now = Instant.now().atZone(ZoneId.of("UTC"))
-        println("%s %s".format(now, now.javaClass))
         val firstState = WetterComState(ZonedDateTime.from(now))
         firstState.addHour(HourState(0, 10.0, 0.05, 0.0, WindDirection.NORTH, 5.0, "Fine", "http://1"))
         firstState.addHour(HourState(1, 12.0, 0.1, 2.0, WindDirection.WEST, 8.0, "Superb", "http://2"))
