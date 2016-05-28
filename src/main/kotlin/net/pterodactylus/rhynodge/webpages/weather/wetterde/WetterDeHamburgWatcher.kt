bda41395386f308d9b40339b50697b4589118acc
David ‘Bombe’ Roden <bombe@pterodactylus.net>
2016-05-28 18:28:36 +0200

    Add watcher for wetter.de

---
+++ src/main/kotlin/net/pterodactylus/rhynodge/webpages/weather/wetterde/WetterDeHamburgWatcher.kt
@@ -0,0 +1,16 @@
+package net.pterodactylus.rhynodge.webpages.weather.wetterde
+
+import net.pterodactylus.rhynodge.filters.HtmlFilter
+import net.pterodactylus.rhynodge.queries.HttpQuery
+import net.pterodactylus.rhynodge.watchers.DefaultWatcher
+
+/**
+ * [DefaultWatcher] implementation that gets the weather for Hamburg.
+ *
+ * @author [David ‘Bombe’ Roden](mailto:bombe@pterodactylus.net)
+ */
+class WetterComHamburgWatcher : DefaultWatcher(
+        HttpQuery("http://www.wetter.de/deutschland/wetter-hamburg-18219464/wetterbericht-aktuell.html"),
+        listOf(HtmlFilter(), WetterDeFilter()),
+        WetterDeTrigger()
+)
